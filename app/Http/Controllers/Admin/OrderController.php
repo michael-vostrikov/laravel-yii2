5507939a4a3d14c42656d508a5efa989c7b0b104
Michael Vostrikov <mv@messapps.com>
2017-07-22 20:42:26 +0500

    Order - CRUD stub

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        return view('admin.order.index');
+    }
+
+    public function view()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin'], function () {
+    Route::get('/order', 'OrderController@index')->name('order.index');
+    Route::get('/order/view/{id}', 'OrderController@view')->name('order.view');
+    Route::get('/order/create', 'OrderController@create')->name('order.create');
+    Route::get('/order/update/{id}', 'OrderController@update')->name('order.update');
+    Route::post('/order/create', 'OrderController@create');
+    Route::post('/order/update/{id}', 'OrderController@update');
+    Route::post('/order/delete/{id}', 'OrderController@delete')->name('order.delete');
+});
